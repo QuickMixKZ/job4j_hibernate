4b6980d9818e3cc058b04a90208b41a31e478611
QuickMixKZ <quickmixkz@gmail.com>
2022-08-12 10:18:59 +0500

    3. LazyInitializationexception [#331987 #331137]

--- src/main/java/ru/job4j/model/onetomany/HbmRunGetList.java
+++ src/main/java/ru/job4j/model/onetomany/HbmRunGetList.java
@@ -6,28 +6,29 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HbmRunGetList {
     public static void main(String[] args) {
+        List<CarBrand> list = new ArrayList<>();
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure().build();
         try {
             SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
             Session session = sf.openSession();
             session.beginTransaction();
-            List<CarBrand> brands = session.createQuery("from CarBrand ").getResultList();
-            for (CarBrand brand : brands) {
-                for (CarModel model : brand.getModels()) {
-                    System.out.println(model);
-                }
-            }
+            list = session.createQuery("select  distinct c from CarBrand c join fetch c.models").getResultList();
+
             session.getTransaction().commit();
             session.close();
         }  catch (Exception e) {
             e.printStackTrace();
         } finally {
             StandardServiceRegistryBuilder.destroy(registry);
+        }
+        for (CarModel model : list.get(0).getModels()) {
+            System.out.println(model);
         }
     }
 }
